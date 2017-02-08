9e27d5f5523f7fea96c04fbd5702ae71a542ad81
David Bisrat <dabisrat@gmail.com>
2017-02-07 23:36:27 -0500

    (feat) add functions that generate random number and operaators

---
+++ node/src/generate-random.js
@@ -0,0 +1,19 @@
+module.exports = {
+  num(){
+    return Math.floor(Math.random() * 101)
+  },
+
+  operator(){
+    const operators = ['+','-','/','*']  
+    const length = operators.length;
+    let randomIndex,temp;
+    // fisher yates shuffle
+    for (let i = 0; i < length; i++) {
+      randomIndex = Math.floor(Math.random() * length)
+      temp = operators[i]
+      operators[i] = operators[randomIndex]
+      operators[randomIndex] = temp
+    }
+    return operators[randomIndex]
+  }
+}
